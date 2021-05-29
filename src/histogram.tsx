1a5272260102770fa81ff1308a14a7842c899ee5
Danny Sivan <dannysi@dropbox.com>
2021-05-29 23:39:09 +0300

    Reading from api

--- src/histogram.tsx
+++ src/histogram.tsx
@@ -1,115 +1,6 @@
 import React from 'react';
 import { processDriveTimes } from './processor';
 
-
-const jsonStr = `[
-  {
-     "startTime": "2015-04-01T12:39:00.000Z",
-     "endTime": "2015-04-01T13:37:00.000Z",
-     "busType": "Urban",
-     "originCode": 12,
-     "destinationCode": 5
-  },
-  {
-     "startTime": "2015-04-01T17:52:00.000Z",
-     "endTime": "2015-04-01T18:50:00.000Z",
-     "busType": "Urban",
-     "originCode": 39,
-     "destinationCode": 7
-  },
-  {
-     "startTime": "2015-04-01T16:36:00.000Z",
-     "endTime": "2015-04-01T17:44:00.000Z",
-     "busType": "Urban Minibus",
-     "originCode": 33,
-     "destinationCode": 18
-  },
-  {
-     "startTime": "2015-04-01T07:28:00.000Z",
-     "endTime": "2015-04-01T08:49:00.000Z",
-     "busType": "Urban Minibus",
-     "originCode": 23,
-     "destinationCode": 32
-  },
-  {
-     "startTime": "2015-04-01T16:15:00.000Z",
-     "endTime": "2015-04-01T17:09:00.000Z",
-     "busType": "Urban",
-     "originCode": 18,
-     "destinationCode": 7
-  },
-  {
-     "startTime": "2015-04-01T07:30:00.000Z",
-     "endTime": "2015-04-01T08:06:00.000Z",
-     "busType": "Urban",
-     "originCode": 44,
-     "destinationCode": 21
-  },
-  {
-     "startTime": "2015-04-01T09:55:00.000Z",
-     "endTime": "2015-04-01T11:38:00.000Z",
-     "busType": "Urban Minibus",
-     "originCode": 25,
-     "destinationCode": 12
-  },
-  {
-     "startTime": "2015-04-01T08:59:00.000Z",
-     "endTime": "2015-04-01T10:12:00.000Z",
-     "busType": "Interurban Minibus",
-     "originCode": 27,
-     "destinationCode": 35
-  },
-  {
-     "startTime": "2015-04-01T13:36:00.000Z",
-     "endTime": "2015-04-01T15:18:00.000Z",
-     "busType": "Urban Minibus",
-     "originCode": 25,
-     "destinationCode": 28
-  },
-  {
-     "startTime": "2015-04-01T16:37:00.000Z",
-     "endTime": "2015-04-01T18:10:00.000Z",
-     "busType": "Urban Minibus",
-     "originCode": 49,
-     "destinationCode": 11
-  },
-  {
-     "startTime": "2015-04-01T19:04:00.000Z",
-     "endTime": "2015-04-01T20:51:00.000Z",
-     "busType": "Interurban Minibus",
-     "originCode": 24,
-     "destinationCode": 47
-  },
-  {
-     "startTime": "2015-04-01T16:49:00.000Z",
-     "endTime": "2015-04-01T17:37:00.000Z",
-     "busType": "Interurban Minibus",
-     "originCode": 30,
-     "destinationCode": 44
-  },
-  {
-     "startTime": "2015-04-01T16:31:00.000Z",
-     "endTime": "2015-04-01T18:00:00.000Z",
-     "busType": "Interurban Minibus",
-     "originCode": 16,
-     "destinationCode": 38
-  },
-  {
-     "startTime": "2015-04-01T23:34:00.000Z",
-     "endTime": "2015-04-02T00:34:00.000Z",
-     "busType": "Urban",
-     "originCode": 27,
-     "destinationCode": 22
-  },
-  {
-     "startTime": "2015-04-01T16:17:00.000Z",
-     "endTime": "2015-04-01T18:17:00.000Z",
-     "busType": "Interurban",
-     "originCode": 7,
-     "destinationCode": 5
-  }
-]`
-
 interface BarProps {
   time: number;
   height: number;
@@ -119,16 +10,23 @@ interface BarProps {
 interface HistogramState {
   bars: Array<BarProps>
 }
+
 export class Histogram extends React.Component<{},HistogramState> {
   state = {bars:[]}
   componentDidMount () {
-    fetch('http://optibus-interview.herokuapp.com/').then(resp => resp.json().then(jsonArray => this.setState({bars:processDriveTimes(jsonArray)})))
+    fetch('proxy/').then(
+      resp => {
+        resp.json().then(
+          jsonArray => this.setState({bars:processDriveTimes(jsonArray)})
+        )
+      }
+    
+    ).catch(reason => console.log(reason))
   }
   render() {
-    // const bars = processDriveTimes(JSON.parse(jsonStr))
     return (
       <div className="histogram">
-        {this.state.bars.map(val => (<Bar {...val} />))}
+        {this.state.bars.map((val, i) => (<Bar key={i} {...val} />))}
       </div>
     )
  }
@@ -143,7 +41,6 @@ class Bar extends React.Component<BarProps> {
       <div className="container"> 
         <div className="amount"> {this.props.height}</div>
         <div className="time"> 
-          {/* <div className="date">{time.getHours() === 0? time.getDate():<div/>} </div> */}
           {time.getHours()}   
         </div>
       </div>
--- src/processor.ts
+++ src/processor.ts
@@ -1,7 +1,5 @@
 const sumDriveTimes = (driveTime: { startTime: number; endTime: number; }, counter: Map<number, number>): void  => {
-    console.log(driveTime)
     for (let i=driveTime.startTime; i<=driveTime.endTime; i+=hourMillis) {
-        console.log(i)  
         const val = counter.get(i)
         counter.set(i,val?val+1:1)
     }
@@ -17,14 +15,10 @@ const findMaxVal = (myMap:Map<any,number>) => {
 }
 const hourMillis = 3600000
 export const processDriveTimes = (jsonArray: Array<any>) => {
-    console.log(jsonArray);
     const driveTimes = jsonArray.map(drive => ({startTime: floorHourMillis(drive.startTime), endTime: ceilHourMillis(drive.endTime)}))
-    //console.log(driveTimes)
     let counter = new  Map<number, number> ()
     driveTimes.forEach(driveTime => sumDriveTimes(driveTime, counter))
-    console.log(counter.size)
     let orderedCounter = new Map<number,number>([...counter.entries()].sort())
     const max = findMaxVal(orderedCounter)
-    console.log(max)
     return [...orderedCounter.entries()].map(entry => ({time:entry[0], height:entry[1], max}))
 }
